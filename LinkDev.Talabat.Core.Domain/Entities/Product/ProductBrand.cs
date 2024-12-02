fb8d55bd0890e7b9bd6216fba7705b6ab2ff30f7
Mohamedosamaselo <mo5195278@gmail.com>
2024-12-02 14:31:17 +0200

    ProductModule _ Entities

---
+++ LinkDev.Talabat.Core.Domain/Entities/Product/ProductBrand.cs
@@ -0,0 +1,13 @@
+﻿namespace LinkDev.Talabat.Core.Domain.Entities.Product
+{
+    public class ProductBrand
+    {
+        public required string Name { get; set; }
+
+
+
+        // Navigational Property [Many]
+        // public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>(); // Reference Looping Issue
+
+    }
+}
---
+++ LinkDev.Talabat.Core.Domain/Entities/Product/ProductCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkDev.Talabat.Core.Domain.Entities.Product
+{
+    public class ProductCategory
+    {
+        public required string Name { get; set; }
+
+
+        //public virtual ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    }
+}
